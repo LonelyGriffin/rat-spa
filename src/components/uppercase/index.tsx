ae32e199bc723c8cd4902ccb91f37b8245f6d10f
Pavel Fanaskov <paul-fanaskov@yandex-team.ru>
2020-02-07 10:46:10 +0300

    cleanup

--- src/components/uppercase/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import css from "./index.module.css";
-
-type InnerProps = {
-    children: React.ReactNode
-}
-
-export const Uppercase = (props: InnerProps) => <span className={css.root}>{props.children}</span>
