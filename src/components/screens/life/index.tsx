ba7a3932cb96587f749142efe8c5b7e3c0942931
Pavel Fanaskov <lonelygriffin@gmail.com>
2020-02-22 11:40:39 +0300

    boxed life points text

--- src/components/screens/life/index.tsx
+++ src/components/screens/life/index.tsx
@@ -21,41 +21,46 @@ export const LifeScreen = (props: Props) => {
             <div className={css.points}>
               <div className={css.points_inner}>
                 <div className={css.point}>
-                  <div className={cn(css.point_content, css.point_content_l, css.point_content_l_1)}>
+                  <div className={css.point_content}>
+                    <div className={cn(css.point_line, css.point_line_l, css.point_line_l_1)}/>
                     <div className={css.point_title_l}>Социум</div>
-                    <div className={css.point_text_l}>
+                    <div className={cn(css.point_text, css.point_text_l)}>
                       Не держать крысу в одиночестве. Одиночки живут меньше. Крысы социальные животные, также достаточный выгул и общение с человеком.
                     </div>
                   </div>
                 </div>
                 <div className={css.point}>
-                  <div className={cn(css.point_content, css.point_content_l, css.point_content_l_2)}>
+                  <div className={css.point_content}>
+                    <div className={cn(css.point_line, css.point_line_l, css.point_line_l_2)}/>
                     <div className={css.point_title_l}>Клетка</div>
-                    <div className={css.point_text_l}>
+                    <div className={cn(css.point_text, css.point_text_l)}>
                       Клетка должна быть достаточно просторной и чистой.
                     </div>
                   </div>
                 </div>
                 <div className={css.point}>
-                  <div className={cn(css.point_content, css.point_content_l, css.point_content_l_3)}>
+                  <div className={css.point_content}>
+                    <div className={cn(css.point_line, css.point_line_l, css.point_line_l_3)}/>
                     <div className={css.point_title_l}>Питание</div>
-                    <div className={css.point_text_l}>
+                    <div className={cn(css.point_text, css.point_text_l)}>
                       Правильное питание Не надо усердствовать во вкусняшках. Не допускать ожирения.
                     </div>
                   </div>
                 </div>
                 <div className={css.point}>
-                  <div className={cn(css.point_content, css.point_content_l, css.point_content_l_2)}>
+                  <div className={css.point_content}>
+                    <div className={cn(css.point_line, css.point_line_l, css.point_line_l_2)}/>
                     <div className={css.point_title_l}>Здоровье</div>
-                    <div className={css.point_text_l}>
+                    <div className={cn(css.point_text, css.point_text_l)}>
                       Своевременный мониторинг состояния здоровья. Своевременное лечение.
                     </div>
                   </div>
                 </div>
                 <div className={css.point}>
-                  <div className={cn(css.point_content, css.point_content_l, css.point_content_l_1)}>
+                  <div className={css.point_content}>
+                    <div className={cn(css.point_line, css.point_line_l, css.point_line_l_1)}/>
                     <div className={css.point_title_l}>Стерилизация</div>
-                    <div className={css.point_text_l}>
+                    <div className={cn(css.point_text, css.point_text_l)}>
                       Кастрация у мальчиков и стерилизация у девочек (если крыса не планируется под разведение) увеличивают продолжительность жизни.
                     </div>
                   </div>
@@ -71,41 +76,46 @@ export const LifeScreen = (props: Props) => {
             <div className={css.points}>
               <div className={css.points_inner}>
                 <div className={css.point}>
-                  <div className={cn(css.point_content, css.point_content_r, css.point_content_r_1)}>
+                  <div className={css.point_content}>
+                    <div className={cn(css.point_line, css.point_line_r, css.point_line_r_1)}/>
                     <div className={css.point_title_r}>Природа</div>
-                    <div className={css.point_text_r}>
+                    <div className={cn(css.point_text, css.point_text_r)}>
                       Продолжительность жизни крысок составляет 2 - 3 года. Это заложено их природой.
                     </div>
                   </div>
                 </div>
                 <div className={css.point}>
-                  <div className={cn(css.point_content, css.point_content_r, css.point_content_r_2)}>
+                  <div className={css.point_content}>
+                    <div className={cn(css.point_line, css.point_line_r, css.point_line_r_2)}/>
                     <div className={css.point_title_r}>Долголетие</div>
-                    <div className={css.point_text_r}>
+                    <div className={cn(css.point_text, css.point_text_r)}>
                       Самая долгоживущая крыска достигла возраста 7 лет - это сравнимо с возрастом в 130 лет у человека.
                     </div>
                   </div>
                 </div>
                 <div className={css.point}>
-                  <div className={cn(css.point_content, css.point_content_r, css.point_content_r_3)}>
+                  <div className={css.point_content}>
+                    <div className={cn(css.point_line, css.point_line_r, css.point_line_r_3)}/>
                     <div className={css.point_title_r}>Породы</div>
-                    <div className={css.point_text_r}>
+                    <div className={cn(css.point_text, css.point_text_r)}>
                       Сфинкс, манкс, альбинос - являются проблемными разновидностями, их продолжительность жизни в среднем на 1 - 0.5 года меньше
                     </div>
                   </div>
                 </div>
                 <div className={css.point}>
-                  <div className={cn(css.point_content, css.point_content_r, css.point_content_r_2)}>
+                  <div className={css.point_content}>
+                    <div className={cn(css.point_line, css.point_line_r, css.point_line_r_2)}/>
                     <div className={css.point_title_r}>Родители</div>
-                    <div className={css.point_text_r}>
+                    <div className={cn(css.point_text, css.point_text_r)}>
                       Только покупка крысят у проверенных заводчиков с описанием родословной может дать какие-то гарантии, в увеличении продолжительности жизни.
                     </div>
                   </div>
                 </div>
                 <div className={css.point}>
-                  <div className={cn(css.point_content, css.point_content_r, css.point_content_r_1)}>
+                  <div className={css.point_content}>
+                    <div className={cn(css.point_line, css.point_line_r, css.point_line_r_1)}/>
                     <div className={css.point_title_r}>Болезни</div>
-                    <div className={css.point_text_r}>
+                    <div className={cn(css.point_text, css.point_text_r)}>
                       Декоративные крысы выведенная в лабораториях разновидность поэтому они изначально носят в себе предрасположенность к каким-либо заболеваниям или патологиям.
                     </div>
                   </div>
