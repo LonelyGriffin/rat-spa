fd6ca7fbb49eb2c235678924641553cbb4425746
Pavel Fanaskov <lonelygriffin@gmail.com>
2020-03-21 08:39:14 +0300

    about icons

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 const css = require('./index.module.css');
 
 const AboutPage = () => (
@@ -7,20 +8,39 @@ const AboutPage = () => (
     <div className={css.content}>
       <div className={css.left}>
         <h2 className={css.subtitle}>О проекте</h2>
-        <p className={css.description}>На этом ресурсе можно получить первое представление о декаративных крысах, особеностях их покупки и содержания. </p>
-        <p className={css.description}>Сайт является не комерческим. Поэтому вы может свободно использовать представленые материалы. При сохранении ссылки на данный ресурс.</p>
+        <p className={css.description}>На этом ресурсе можно получить первое представление о декаративных крысах,
+          особеностях их покупки и содержания. </p>
+        <p className={css.description}>Сайт является не комерческим. Поэтому вы может свободно использовать
+          представленые материалы. При сохранении ссылки на данный ресурс.</p>
       </div>
       <div className={css.right}>
         <h2 className={css.subtitle}>Контакты</h2>
         <dl className={css.contacts}>
           <dt>Разработку вел:</dt>
-          <dd><a className={css.link} href="mailto:lonelygriffin@gmail.com">LonelyGriffin</a></dd>
+          <dd>
+            <a className={css.link} href="mailto:lonelygriffin@gmail.com">
+              <div className={css.mail_icon}/>LonelyGriffin
+            </a>
+          </dd>
           <dt>Дизайн рисовала:</dt>
-          <dd><a className={css.link} href="mailto:kateartprint@gmail.com">Kateartprint</a></dd>
+          <dd>
+            <a className={css.link} href="mailto:kateartprint@gmail.com">
+              <div className={css.mail_icon}/>Kateartprint
+            </a>
+          </dd>
           <dt>Исходный код доступен здесь:</dt>
-          <dd><a className={css.link} href="https://github.com/LonelyGriffin/rat-spa">Github</a></dd>
+          <dd>
+            <a className={css.link} href="https://github.com/LonelyGriffin/rat-spa">
+              <div className={css.github_icon}/>
+              Github
+            </a>
+          </dd>
           <dt>Контакт для любых вопросов:</dt>
-          <dd><a className={css.link} href="mailto:lonelygriffin@gmail.com">lonelygriffin@gmail.com</a></dd>
+          <dd>
+            <a className={css.link} href="mailto:lonelygriffin@gmail.com">
+              <div className={css.mail_icon}/>lonelygriffin@gmail.com
+            </a>
+          </dd>
         </dl>
       </div>
     </div>
