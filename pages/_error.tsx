4bab6ff994c46f9eb8744a520a493af15c0c9900
Pavel Fanaskov <lonelygriffin@gmail.com>
2020-03-23 18:19:22 +0300

    error screen design

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,10 +1,23 @@
+const css = require('./_error.module.css');
+
 function Error({ statusCode }: any) {
   return (
-    <p>
-      {statusCode
-        ? `An error ${statusCode} occurred on server`
-        : 'An error occurred on client'}
-    </p>
+    <div className={css.root}>
+      <div className={css.header}>
+        <h1 className={css.title}>
+        {statusCode
+          ? `${statusCode}`
+          : ''}
+        </h1>
+      </div>
+      <div className={css.center}>
+       <div className={css.img} />
+      </div>
+      <div className={css.footer}>
+        <p className={css.description}>Ой, похоже мы что то потеряли. Попробуйте начать с начала.</p>
+        <a className={css.link} href={'/'}>Вернуться на главную ➜</a>
+      </div>
+    </div>
   )
 }
 
