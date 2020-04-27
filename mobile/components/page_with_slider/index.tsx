60216cad0cf14ac3eba3d1f278081fd06c0decf1
Pavel Fanaskov <paul-fanaskov@yandex-team.ru>
2020-04-27 07:29:17 +0300

    text scroll

--- mobile/components/page_with_slider/index.tsx
+++ mobile/components/page_with_slider/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { TDataNode } from '../../model/screen'
 import { ImageCarousel } from '../image_carousel'
 import css from './index.module.css'
+import { Swipeable, EventData } from 'react-swipeable'
+import { useRef } from 'react';
 
 type Props = {
   data: TDataNode
@@ -10,6 +12,8 @@ type Props = {
 
 export const PageWithSlider = (props: Props) => {
   const {title, text, images, imagesCursor} = props.data
+  const isVerticalSwipeRef = useRef(false)
+  const isFirstSwipeEvent = useRef(true)
 
   const setImagesCursor = (newImageCursor: number) => {
     props.setData({...props.data, imagesCursor: newImageCursor})
@@ -17,15 +21,40 @@ export const PageWithSlider = (props: Props) => {
 
   const initialImagesCursor = images && images.length > 1 ? 1 : 0
 
+  const contnetSwipingHandler = (e: EventData) => {
+    if(isFirstSwipeEvent.current) {
+      isFirstSwipeEvent.current = false
+      if (e.dir === 'Up' || e.dir === 'Down') {
+        isVerticalSwipeRef.current = true
+      }
+    }
+
+    if (isVerticalSwipeRef.current) {
+      e.event.stopPropagation()
+    }
+  }
+  const contnetSwipedHandler = (e: EventData) => {
+    if (isVerticalSwipeRef.current) {
+      e.event.stopPropagation()
+    }
+
+    isFirstSwipeEvent.current = true
+    isVerticalSwipeRef.current = false
+  }
+
   return (
     <div className={css.root}>
       <div className={css.images}>
         {images && <ImageCarousel images={images} active={imagesCursor !== undefined ? imagesCursor : initialImagesCursor} setActive={setImagesCursor}/>}
       </div>
-      <div className={css.content}>
+      <Swipeable
+        onSwiping={contnetSwipingHandler}
+        onSwiped={contnetSwipedHandler}
+        className={css.content}
+      >
         <h3 className={css.title}>{title}</h3>
         <p className={css.text}>{text}</p>
-      </div>
+      </Swipeable>
     </div>
   )
 }
